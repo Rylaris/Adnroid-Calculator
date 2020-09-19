11a90175a45369aa7cc5af05d9df764883906098
温蟾圆 <1052403874@outlook.com>
2020-09-19 19:25:21 +0800

    feat: 添加正弦余弦计算功能

--- app/src/main/java/com/example/calculator/FunctionActivity.java
+++ app/src/main/java/com/example/calculator/FunctionActivity.java
@@ -9,14 +9,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.*;
 
 public class FunctionActivity extends AppCompatActivity {
+
+    Button sinButton, cosButton;
+    TextView sinResult, cosResult;
+    EditText sinValue, cosValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function);
+        initComponents();
+    }
 
+    private void initComponents() {
+        sinButton = findViewById(R.id.sin_button);
+        cosButton = findViewById(R.id.cos_button);
+        sinResult = findViewById(R.id.sinResult_text);
+        cosResult = findViewById(R.id.cosResult_text);
+        sinValue = findViewById(R.id.sinValue_text);
+        cosValue = findViewById(R.id.cosValue_text);
+
+        sinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sin();
+            }
+        });
+
+        cosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cos();
+            }
+        });
+    }
+
+    private void sin() {
+        String val = String.valueOf(sinValue.getText());
+        if (val.isEmpty()) {
+            return;
+        }
+        Double value = Double.parseDouble(String.valueOf(sinValue.getText()));
+        Double result = Math.sin(value);
+        sinResult.setText("=" + result.toString());
+    }
+
+    private void cos() {
+        String val = String.valueOf(cosValue.getText());
+        if (val.isEmpty()) {
+            return;
+        }
+        Double value = Double.parseDouble(String.valueOf(cosValue.getText()));
+        Double result = Math.cos(value);
+        cosResult.setText("=" + result.toString());
     }
 
 }
--- app/src/main/java/com/example/calculator/test/MainTest.java
+++ app/src/main/java/com/example/calculator/test/MainTest.java
@@ -5,26 +5,6 @@ import com.example.calculator.Calculate;
 
 public class MainTest {
     public static void main(String[] args) {
-        Calculate calculate = new Calculate();
-        calculate.delete();
-        calculate.appendNum(1);
-        calculate.arithmetic(ButtonState.PLUS);
-        calculate.appendNum(2);
-        calculate.delete();
-        calculate.appendNum(3);
-        calculate.decimal();
-        calculate.appendNum(3);
-        calculate.equals();
-        calculate.clear();
-        calculate.appendNum(1);
-        calculate.arithmetic(ButtonState.PLUS);
-        calculate.appendNum(2);
-        calculate.delete();
-        calculate.appendNum(3);
-        calculate.percent();
-        calculate.arithmetic(ButtonState.PLUS);
-        calculate.appendNum(3);
-        calculate.arithmetic(ButtonState.MUTIPLY);
-        System.out.println(calculate.getAnswer());
+        System.out.print(Math.sin(3.14));
     }
 }
